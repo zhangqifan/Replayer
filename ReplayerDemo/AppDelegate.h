f26307596b994e537f2abf257363f25afde4437b
zhangqifan <qifan.zhang@chipmuck.cc>
2017-06-26 10:27:39 +0800

    Initial Commit

---
+++ ReplayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReplayerDemo
+//
+//  Created by qifan.zhang on 2017/6/26.
+//  Copyright © 2017年 qifan.zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
